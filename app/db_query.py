cbaaee64826ec4c6f1d1e34b740a21af6c826fe5
Pat T <pipat.th@gmail.com>
2019-03-30 14:08:21 -0400

    address little changes suggested in PR#5

--- app/db_query.py
+++ app/db_query.py
@@ -97,27 +97,27 @@ def get_network_by_id(starting_pmid, citation_depth=1):
     # iterate through the depth, starting with starting_pmid, store dataframe result in a dict
     l_ = []
 
-    # going forward
-    depth_iterations = 1
+    # outgoing
+    depth_iteration = 1
     ids = [starting_pmid]
-    while depth_iterations <= citation_depth:
+    while depth_iteration <= citation_depth:
         df_citation_out, n = get_citations_by_id(ids, id_type='from')
-        df_citation_out['depth'] = depth_iterations
+        df_citation_out['depth'] = depth_iteration
         # add the current batch of nodes to the master list...
         ids = df_citation_out['bpmid'].tolist()
         l_.append(df_citation_out)
-        depth_iterations += 1
+        depth_iteration += 1
 
-    # going backward
-    depth_iterations = 1
+    # incoming
+    depth_iteration = 1
     ids = [starting_pmid]
-    while depth_iterations <= citation_depth:
+    while depth_iteration <= citation_depth:
         df_citation_in, n = get_citations_by_id(ids, id_type='to')
-        df_citation_in['depth'] = depth_iterations
+        df_citation_in['depth'] = depth_iteration
         # add the current batch of nodes to the master list...
         ids = df_citation_in['apmid'].tolist()
         l_.append(df_citation_in)
-        depth_iterations += 1
+        depth_iteration += 1
 
     # combine all iterations
     return pd.concat(l_, ignore_index=True)
